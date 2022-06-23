4ba817cad46cdf938bdc272b34e9a0ab938c8a9a
Yogesh Yashwant Gurav <Yogeshgu@cpbs.net>
2022-06-23 11:23:45 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WeatherInfoService } from './weather-info.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Weather-Forecast-Mini-App';
-  public cityList =['Banff','Brooks','Calgary','Edmonton','Fort McMurray','Grande Prairie','Toronto'];
+  public cityList =['Calgary','Edmonton','Fort McMurray','Grande Prairie','Toronto'];
   public dropdownSelect: any="";
   public featchedData: any;
   public iconUrl:string="../../assets/icon/";
 
 constructor(public service:WeatherInfoService){}
+  ngOnInit(): void {  
+   if(this.cityList){    
+    this.dropdownSelect=this.cityList[0];
+    this.service.getWeathetForecastInfo(this.dropdownSelect).subscribe(res=>{
+    this.forecastInformation(res);
+    });
+   }
+  }
 
+ 
 public weathetResponseInfo(){  
-if(this.dropdownSelect){
-  this.service.getWeathetForecastInfo(this.dropdownSelect).subscribe(res=>{  
+if(this.dropdownSelect){ 
+  this.service.getWeathetForecastInfo(this.dropdownSelect).subscribe(res=>{    
     this.forecastInformation(res);
   });
 }
